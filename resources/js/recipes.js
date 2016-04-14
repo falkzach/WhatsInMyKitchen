6feb3510e9c73e5ec773976fea2c03b33608bd86
Zachary Falkner <falkzach@gmail.com>
2016-04-13 19:16:27 -0600

    add classes, use map

--- resources/js/recipes.js
+++ resources/js/recipes.js
@@ -4,17 +4,18 @@ var recipes = {
         recipes._add_initial_recipes();
     },
 
-    _add_initial_recipes:function()
+    _add_initial_recipes: function()
     {
+        var items = ["Cupcakes", "Cookies", "Sushi", "Apple Pie"];
         var ul = document.getElementById("recipes_list");
-        recipes._add_recipe("Cupcakes", ul);
-        recipes._add_recipe("Cookies", ul);
-        recipes._add_recipe("Sushi", ul);
-        recipes._add_recipe("Apple Pie", ul);
+        items.map(function(item){
+            recipes._add_recipe(item,ul)
+        });
     },
 
-    _add_recipe:function(name, ul){
+    _add_recipe: function(name, ul){
         var li = document.createElement("li");
+        li.className = 'list-group-item';
         li.appendChild(document.createTextNode(name));
         ul.appendChild(li);
 
