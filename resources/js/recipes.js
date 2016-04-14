7bb43015ff1ca5ad85c8d45e62d7197421d58975
Zachary Falkner <falkzach@gmail.com>
2016-04-13 20:19:31 -0600

    add recipe example

--- resources/js/recipes.js
+++ resources/js/recipes.js
@@ -4,8 +4,7 @@ var recipes = {
         recipes._add_initial_recipes();
     },
 
-    _add_initial_recipes: function()
-    {
+    _add_initial_recipes: function() {
         var items = ["Cupcakes", "Cookies", "Sushi", "Apple Pie"];
         items.map(function(item){
             var context = {name: item};
@@ -18,7 +17,11 @@ var recipes = {
             var html_string = template.get_html(t, context);
             $('#recipes_list').append(html_string);
         });
-
     },
+
+    add_recipe: function(name) {
+        var context = {name: name};
+        recipes._add_recipe_li(context);
+    }
 
 };
