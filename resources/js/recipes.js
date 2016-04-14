31aefc6031e044478d33861cd603f4f3c6914a10
Zachary Falkner <falkzach@gmail.com>
2016-04-13 20:14:38 -0600

    clean up cruft

--- resources/js/recipes.js
+++ resources/js/recipes.js
@@ -2,40 +2,23 @@ var recipes = {
     init: function()
     {
         recipes._add_initial_recipes();
-
     },
 
     _add_initial_recipes: function()
     {
         var items = ["Cupcakes", "Cookies", "Sushi", "Apple Pie"];
-        var ul = document.getElementById("recipes_list");
         items.map(function(item){
-            //recipes._add_recipe(item,ul)
-
             var context = {name: item};
-            recipes._recipe_ul(context);
+            recipes._add_recipe_li(context);
         });
     },
 
-    _add_recipe: function(name, ul){
-        var li = document.createElement("li");
-        li.className = 'list-group-item';
-        li.appendChild(document.createTextNode(name));
-        ul.appendChild(li);
-
-    },
-
-    _recipe_ul:function(context){
+    _add_recipe_li:function(context){
         template.get_template('resources/templates/recipe_li.handlebars', function(t) {
             var html_string = template.get_html(t, context);
             $('#recipes_list').append(html_string);
         });
 
     },
-
-
-
-
-
 
 };
