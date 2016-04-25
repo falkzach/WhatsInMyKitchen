e9b2aa1e17371fde54b123bb5d40864ddde3da11
Zachary Falkner <falkzach@gmail.com>
2016-04-25 15:11:10 -0600

    change inputs on kitchen numbers

--- resources/js/template.js
+++ resources/js/template.js
@@ -11,31 +11,7 @@ var template = {
             url: path,
             success: function (data) {
                 source = data;
-
-                Handlebars.registerHelper('amountList', function(name, amount, options) {
-                    var ret;
-
-                    var start = parseInt(amount) - 5;
-
-                    while(parseInt(start) < 1) start++;
-
-                    var newAmount = 0;
-                    var selected = "";
-
-                    for(var i=0; i<10; i++) {
-                        newAmount = parseInt(parseInt(start) + parseInt(i));
-                        if(amount == newAmount)
-                            selected = "selected";
-                        else
-                            selected = "";
-                        ret = ret + "<option " + selected + "><a href='#' >" + newAmount + "</a></option>";
-                    }
-
-                    return ret;
-                });
-
                 template = Handlebars.compile(source);
-
                 if (callback) callback(template);
             }
         });
